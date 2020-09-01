5ef81b69631cdb4d145b483d67195099885f1914
孔团兵 <kongtuanbing@cnpc.com.cn>
2020-09-01 19:44:48 +0800

    update 更新login

---
+++ react-admin-project/src/page/login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './login.scss'
+class About extends Component {
+  render (){
+    return (
+      <div className="full-page login-page">
+        <div className="login-from"></div>
+      </div>
+    )
+  }
+}
+
+export default About;
--- react-admin-project/src/router/index.js
+++ react-admin-project/src/router/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {HashRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import AsyncLazyLoad from '../components/AsyncLazyLoad.js';
-
+const Login = AsyncLazyLoad(() => import('../page/login/Login.js'));
 const Home = AsyncLazyLoad(() => import('../page/home/Home.js'));
 const About = AsyncLazyLoad(() => import('../page/about/About.js'));
 const ErrorPage = AsyncLazyLoad(() => import('../page/error/Error.js'));
@@ -12,7 +12,8 @@ class RouterConfig extends Component {
       <Router>
         <div className="root-router-page">
           <Switch>
-            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/" component={Login}></Route>
+            <Route path="/login" component={Login}></Route>
             <Route path="/home" component={Home}></Route>
             <Route path="/about" component={About}></Route>
             <Route path="/error" component={ErrorPage}></Route>
