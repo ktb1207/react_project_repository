7dfe948833395190d47a625642e4b17d53a4d48a
kongtb <13103820893@163.com>
2020-09-24 23:21:58 +0800

    菜单修改01

--- react-admin-project/src/components/leftMenus/LeftMenus.js
+++ react-admin-project/src/components/leftMenus/LeftMenus.js
@@ -6,7 +6,6 @@ import {
   MenuFoldOutlined,
   PieChartOutlined,
   DesktopOutlined,
-  ContainerOutlined,
   MailOutlined,
 } from '@ant-design/icons';
 import './LeftMenus.scss';
@@ -14,17 +13,15 @@ import './LeftMenus.scss';
 const { SubMenu } = Menu;
 
 function LeftMenus() {
+  // 展开/折叠
   const [collapsed, setCollapsed] = useState(false)
-  
+  // 展开/折叠-控制
   const toggleCollapsed = () => {
     setCollapsed(!collapsed)
   }
 
   return (
     <div className="left-meuns-components">
-      <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
-        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
-      </Button>
       <Menu
         defaultSelectedKeys={['1']}
         defaultOpenKeys={['sub1']}
@@ -33,29 +30,19 @@ function LeftMenus() {
         inlineCollapsed={collapsed}
       >
         <Menu.Item key="1" icon={<PieChartOutlined />}>
-          Option 1
+          用户管理
         </Menu.Item>
         <Menu.Item key="2" icon={<DesktopOutlined />}>
-          Option 2
+          订单管理
         </Menu.Item>
-        <Menu.Item key="3" icon={<ContainerOutlined />}>
-          Option 3
-        </Menu.Item>
-        <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-          <Menu.Item key="5">Option 5</Menu.Item>
-          <Menu.Item key="6">Option 6</Menu.Item>
-          <Menu.Item key="7">Option 7</Menu.Item>
-          <Menu.Item key="8">Option 8</Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-          <Menu.Item key="9">Option 9</Menu.Item>
-          <Menu.Item key="10">Option 10</Menu.Item>
-          <SubMenu key="sub3" title="Submenu">
-            <Menu.Item key="11">Option 11</Menu.Item>
-            <Menu.Item key="12">Option 12</Menu.Item>
-          </SubMenu>
+        <SubMenu key="sub1" icon={<MailOutlined />} title="商家管理">
+          <Menu.Item key="5">运营商管理</Menu.Item>
+          <Menu.Item key="6">设备管理</Menu.Item>
         </SubMenu>
       </Menu>
+      <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
+        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+      </Button>
     </div>
   )
 }
