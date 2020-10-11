f5454baeef8f47f3bef255156b53efea58280429
kongtb <13103820893@163.com>
2020-10-11 23:10:01 +0800

    修改错误提醒

--- react-admin-project/src/components/renderInDom/RenderInDom.js
+++ react-admin-project/src/components/renderInDom/RenderInDom.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import ReactDom from 'react-dom';
 import PropTypes from 'prop-types';
 import util from '../../utils/utils';
