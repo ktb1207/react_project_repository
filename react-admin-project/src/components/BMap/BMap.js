059c4938bb1a2cbffbc9b7089a1329209df8d81d
孔团兵 <kongtuanbing@cnpc.com.cn>
2020-10-27 17:07:49 +0800

    update 百度地图选点

--- react-admin-project/src/components/BMap/BMap.js
+++ react-admin-project/src/components/BMap/BMap.js
@@ -8,6 +8,11 @@ function Map () {
     const map = new BMap.Map("bmap");
     // 初始化地图,用城市名设置地图中心点
     map.centerAndZoom("北京", 11);
+    //开启鼠标滚轮缩放
+    map.enableScrollWheelZoom(true);  
+    map.addEventListener('click', function (e) {
+      console.log('点击经纬度：' + e.point.lng + ',' + e.point.lat)
+    }); 
   },[])
   return (
     <div className="b-map" id="bmap"></div>
