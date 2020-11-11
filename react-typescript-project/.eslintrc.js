151e274347408bfe6e731b3ce923dfc20734293d
孔团兵 <kongtuanbing@cnpc.com.cn>
2020-11-11 13:38:50 +0800

    update 更新配置

--- react-typescript-project/.eslintrc.js
+++ react-typescript-project/.eslintrc.js
@@ -1,26 +1,28 @@
-module.exports =  {
-    parser:  '@typescript-eslint/parser',  // 指定ESLint解析器
-    extends:  [
-      'plugin:react/recommended',  // 使用来自 @eslint-plugin-react 的推荐规则
-      'plugin:@typescript-eslint/recommended',  // 使用来自@typescript-eslint/eslint-plugin的推荐规则
-      'prettier/@typescript-eslint',  // 使用 ESLint -config-prettier 禁用来自@typescript-eslint/ ESLint 与 prettier 冲突的 ESLint 规则
-      'plugin:prettier/recommended', 
-    ],
-    parserOptions:  {
-      ecmaVersion:  2018,  // 允许解析最新的 ECMAScript 特性
-      sourceType:  'module',  // 允许使用 import
-      ecmaFeatures:  {
-        jsx:  true,  // 允许对JSX进行解析
-      },
-    },
-    rules:  {
-      // 自定义规则
-      // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    },
-    settings:  {
-      react:  {
-        version:  'detect',  // 告诉 eslint-plugin-react 自动检测 React 的版本
-      },
-    },
-  };
-  
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser', // 指定ESLint解析器
+  // 此项是用来配置标准的js风格
+  extends: [
+    'plugin:react/recommended', // 使用来自 @eslint-plugin-react 的推荐规则
+    'plugin:@typescript-eslint/recommended', // 使用来自@typescript-eslint/eslint-plugin的推荐规则
+    'prettier/@typescript-eslint', // 使用 ESLint -config-prettier 禁用来自@typescript-eslint/ ESLint 与 prettier 冲突的 ESLint 规则
+    'plugin:prettier/recommended'
+  ],
+  //此项是用来指定javaScript语言类型和风格，sourceType用来指定js导入的方式，默认是script，此处设置为module，指某块导入方式
+  parserOptions: {
+    ecmaVersion: 2018, // 允许解析最新的 ECMAScript 特性
+    sourceType: 'module', // 允许使用 import
+    ecmaFeatures: {
+      jsx: true // 允许对JSX进行解析
+    }
+  },
+  rules: {
+    // 自定义规则
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    react: {
+      version: 'detect' // 告诉 eslint-plugin-react 自动检测 React 的版本
+    }
+  }
+};
