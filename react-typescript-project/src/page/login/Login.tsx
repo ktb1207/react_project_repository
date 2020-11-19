70067b34456bdaf316ec678a3bc6ff5c89a9d663
kongtb <13103820893@163.com>
2020-11-19 23:06:42 +0800

    update 登录受控input验证完成

--- react-typescript-project/src/page/login/Login.tsx
+++ react-typescript-project/src/page/login/Login.tsx
@@ -1,12 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, ChangeEvent } from 'react';
 import './login.scss';
 import { Button } from 'antd-mobile';
-class Login extends Component {
+
+interface IProps {
+  name?: string;
+}
+
+interface IState {
+  inputValue: string;
+}
+class Login extends Component<IProps, IState> {
+  static defaultProps = {
+    name: '登录'
+  };
+
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      inputValue: '我是初始化值'
+    };
+  }
+  inputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    console.log(event);
+    this.setState({
+      inputValue: event.target.value
+    });
+  };
   render(): React.ReactElement {
+    const buttonName: string | undefined = this.props.name;
     return (
       <div className="full-page login-page">
         <div className="content-center">
-          <Button type="primary">login</Button>
+          <input type="text" value={this.state.inputValue} onChange={this.inputChange} />
+          <Button type="primary">{buttonName}</Button>
         </div>
       </div>
     );
