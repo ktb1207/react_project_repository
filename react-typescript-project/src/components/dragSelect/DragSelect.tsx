be88b7d3a7bfaef119521f2da291cd75ee7c8ad0
kongtb <13103820893@163.com>
2020-12-04 23:19:46 +0800

    update 完善滑动组件

--- react-typescript-project/src/components/dragSelect/DragSelect.tsx
+++ react-typescript-project/src/components/dragSelect/DragSelect.tsx
@@ -2,27 +2,29 @@ import './dragSelect.scss';
 import React, { useState } from 'react';
 
 const spotWidth: number = 6;
-const lineEndsMargin: number = 10;
-const squreWidth: number = 15;
+const lineEndsMargin: number = 10; // 标度线左右边距
+const squreWidth: number = 20; // 滑块宽
 const squreMargin: number = lineEndsMargin - squreWidth / 2;
 
 const DragSelect: React.FC = () => {
   const [squreLeftDistance, setSqureLeftDistance] = useState<number>(0);
   const [maxDragLeft, setMaxDragLeft] = useState<number>(0);
   // 开始滑动
+  // 获得滑块可滑动最大偏移量
   const squreTouchStart = (event: React.TouchEvent<HTMLDivElement>): void => {
     const $target = event.target as HTMLDivElement;
     const parentEl = $target.parentElement;
     const $parentW = window.getComputedStyle(parentEl as HTMLDivElement, null).width;
-    setMaxDragLeft(parseInt($parentW) - squreWidth - 2);
+    setMaxDragLeft(parseInt($parentW) - squreWidth);
   };
   // 滑动中
   const squreTouchMove = (event: React.TouchEvent<HTMLDivElement>): void => {
-    console.log(event);
     let scollNum = event.changedTouches[0].clientX;
+    // 最小
     if (scollNum <= 0) {
       scollNum = 0;
     }
+    // 最大
     if (scollNum >= maxDragLeft) {
       scollNum = maxDragLeft;
     }
