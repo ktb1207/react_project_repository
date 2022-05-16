dc4795fb2dbe35f5978bb480f95bb70e1d249c57
kongtb <13103820893@163.com>
2022-05-16 22:10:05 +0800

    no commit

--- react-admin-project/src/components/AsyncLazyLoad.js
+++ react-admin-project/src/components/AsyncLazyLoad.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import NProgress from 'nprogress';
-export default function AsyncLazyLoad (componentfn) {
+export default function AsyncLazyLoad(componentfn) {
   class LazyLoadComponent extends Component {
-    constructor (props) {
-      super(props)
+    constructor(props) {
+      super(props);
       this.state = {
-        component:null
-      }
+        component: null,
+      };
     }
-    async componentDidMount () {
-      NProgress.start()
-      const {default: component} = await componentfn();
-      this.setState({component})
+    async componentDidMount() {
+      NProgress.start();
+      const { default: component } = await componentfn();
+      this.setState({ component });
       NProgress.done();
     }
     render() {
       const C = this.state.component;
-      return C ? <C {...this.props}/> : null;
+      return C ? <C {...this.props} /> : null;
     }
   }
   return LazyLoadComponent;
