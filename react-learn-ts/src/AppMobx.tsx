2b0585d063894b9c1a2cb4e64dbcbddf95fe1dda
kongtuanb <kongtuanb@chanjet.com>
2022-07-19 23:16:04 +0800

    add link smile-ui 用法

--- react-learn-ts/src/AppMobx.tsx
+++ react-learn-ts/src/AppMobx.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ClassPage from './pages/MobxDemo/ClassPage';
 import FnPage from './pages/MobxDemo/FnPage';
+import { Button } from 'smile-ui';
 function AppMobx() {
   return (
     <div>
       <h3>mobx class组件用法</h3>
       <ClassPage></ClassPage>
       <hr />
+      <Button theme="success">我的按钮</Button>
       <h3>mobx function组件用法</h3>
       <FnPage></FnPage>
     </div>
